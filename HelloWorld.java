f19908651de84d33b4fe65c07027656d8c3dd79d
Alex De Meo <alexdm9572@gmail.com>
2023-02-06 19:21:30 +0000

    Finished

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+* This program displays text to the user
+*
+* author  Alex De Meo
+* version 1.0
+* since   2023/02/06
+*/
+class HelloWorld {
+    public static void main(String[] args){
+        // printing to screen
+        System.out.println("Hello Nicolas Riscalas");
+        System.out.println("How are you doing today brother?");
+    }
+}
+
+
